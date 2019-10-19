7fb6afbbc8107d8fd79a17ba64cbb845b12b8cc0
F2020W <525753150@qq.com>
2019-10-19 16:37:51 +0800

    13

--- src/main/java/com/csto/homework/mapper/course/CourseInfoMapper.java
+++ src/main/java/com/csto/homework/mapper/course/CourseInfoMapper.java
@@ -2,7 +2,7 @@ package com.csto.homework.mapper.course;
 
 import com.csto.homework.entity.course.CourseInfo;
 import org.apache.ibatis.annotations.Mapper;
-
+import org.apache.ibatis.annotations.Param;
 
 
 /**
@@ -17,5 +17,11 @@ public interface CourseInfoMapper {
 
     //老师创建课程
     int createCourse(CourseInfo courseInfo);
+
+    //查询数据库中是否有该老师添加的课程名
+    int findCourseByCourseName(@Param("userInfoId") int uid, @Param("courseName") String courseName);
+
+    //查询老师已经添加了的全部课程
+
 
 }
